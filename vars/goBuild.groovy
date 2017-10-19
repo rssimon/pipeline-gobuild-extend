bc9e88b947dd56263a96167b1c5409bbffc43d10
Rijo Simon <rssimon@alaska.edu>
2017-10-19 01:30:11 -0700

    changes to goBuild.groovy

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -1,5 +1,8 @@
 #!/usr/bin/env groovy
 
 def call() {
-  gobuild product: 'copytest64', branch: 'stage', changeset: 'latest', buildType: 'beta'
+node {
+ stage 'Stage 1'
+ gobuild product: 'copytest64', branch: 'stage', changeset: 'latest', buildType: 'beta'
+}
 }
