36c612a3587552ecd4e2d5ae026c0b63336da0c5
Rijo Simon <rssimon@alaska.edu>
2017-10-19 01:40:34 -0700

    changes to goBuild.groovy

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -1,8 +1,13 @@
 #!/usr/bin/env groovy
 
-def call() {
-node {
- stage 'Stage 1'
- gobuild product: 'copytest64', branch: 'stage', changeset: 'latest', buildType: 'beta'
-}
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    product=config.product
+    node {
+     stage 'Stage 1'
+     gobuild product: '${product}', branch: 'stage', changeset: 'latest', buildType: 'beta'
+    }
 }
