0029b6eb92ce131dd58b003405a88a7cecac4b39
Rijo Simon <rssimon@alaska.edu>
2017-10-23 10:37:13 -0700

    Adding argument to RunGoBuildWithSomething

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -4,6 +4,6 @@ def RunGoBuildWithSomething(product, branch, changeset, buildType)
 {
 node {
  stage 'go-build-stage'
- gobuild product: ${product}, branch: ${branch}, changeset: ${changeset}, buildType: ${buildType}
+ gobuild product: "${product}", branch: "${branch}", changeset: "${changeset}", buildType: "${buildType}"
 }
 }
