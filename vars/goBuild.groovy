64d4c9708aa5e6fe1fb4475d54d3b17f026b1656
Rijo Simon <rssimon@alaska.edu>
2017-10-23 10:32:56 -0700

    Adding argument to RunGoBuildWithSomething

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -1,17 +1,9 @@
 #!/usr/bin/env groovy
 
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    node {
-     stage 'go-build-stage'
-     gobuild product: "${config.product}", branch: "${config.branch}", changeset: "${config.changeset}", buildType: "${config.buildType}"
-    }
-}
-
-def RunGoBuildWithSomething(arg1, arg2)
+def RunGoBuildWithSomething(product, branch, changeset, buildType)
 {
-  echo "Hello World GoBuild with Something. ${arg1}.${arg2}"
+node {
+ stage 'go-build-stage'
+ gobuild product: ${product}, branch: ${branch}, changeset: ${changeset}, buildType: ${buildType}
+}
 }
