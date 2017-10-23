d2bd962ecc3bd9ae9de681646d2f5b0fc726ff0f
Rijo Simon <rssimon@alaska.edu>
2017-10-23 10:39:29 -0700

    Adding argument to RunGoBuildWithSomething

--- vars/goBuild.groovy
+++ vars/goBuild.groovy
@@ -1,9 +1,17 @@
 #!/usr/bin/env groovy
 
-def RunGoBuildWithSomething(product, branch, changeset, buildType)
+def RunGoBuild(product, branch, changeset, buildType)
+{
+  node {
+   stage 'go-build-stage'
+   gobuild product: "${product}", branch: "${branch}", changeset: "${changeset}", buildType: "${buildType}"
+  }
+}
+
+def RunGoBuildWithLatestChange(product, branch, buildType)
 {
 node {
  stage 'go-build-stage'
- gobuild product: "${product}", branch: "${branch}", changeset: "${changeset}", buildType: "${buildType}"
+ gobuild product: "${product}", branch: "${branch}", changeset: "latest", buildType: "${buildType}"
 }
 }
